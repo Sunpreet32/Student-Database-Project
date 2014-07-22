1fcfe8af283d254d33f605287db54047341a4e6c
Sunpreet Singh <sunpreet32@outlook.com>
2014-07-22 15:12:48 +0530

    Update first_login.java

--- first_login.java
+++ first_login.java
@@ -1,15 +1,8 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package stud_database;
 
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author Sumer Singh
- */
 public class first_login extends javax.swing.JFrame {
 
     /**
